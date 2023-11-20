946d99da044dc6eccc4077645c3d4f192c838515
wukdddang <dnr8874@naver.com>
2023-11-20 18:10:16 +0900

    Feat: 타입 추론 학습

---
+++ 1_basic/5_type_inference.ts
@@ -0,0 +1,47 @@
+let stringType = "string";
+let booleanType = true;
+let numberType = 30;
+
+booleanType = false;
+
+const constStringType = "const string";
+const constBooleanType = true;
+
+let yuJin = {
+  name: "안유진",
+  age: 2003,
+};
+
+const yuJin2 = {
+  name: "안유진",
+  age: 2003,
+};
+
+const yuJin3 = {
+  name: "안유진" as const,
+  age: 2003 as const,
+};
+
+yuJin3.name = "안유진";
+
+console.log(yuJin3.name);
+
+/**
+ * Array
+ */
+
+let numbers = [1, 2, 3, 4, 5];
+let numbersAndString = [1, 2, 3, "4", "5", "6"];
+
+// numbers.push(6);
+// numbers.push('6');
+const number = numbers[0];
+const nos = numbersAndString[0];
+const nos2 = numbersAndString[100];
+
+// tuple
+const twoNumbers = [1, 3] as const;
+
+// twoNumbers[0] = 10;
+// twoNumbers.push(100);
+const first = twoNumbers[0];
