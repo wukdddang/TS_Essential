747fbd32d1dc25738d65c6566297a8412dc7b316
wukdddang <dnr8874@naver.com>
2024-02-27 22:39:14 +0900

    Feat: udemy ts 강의 학습 - 36 / 339

---
+++ features/tuples.ts
@@ -0,0 +1,7 @@
+const drink = { color: "brown", carbonated: true, sugar: 40 };
+
+type Drink = [string, boolean, number]; // This is a tuple
+
+const pepsi: Drink = ["brown", true, 40]; // This is an array
+const sprite: Drink = ["clear", true, 40];
+const tea: Drink = ["brown", false, 0];
