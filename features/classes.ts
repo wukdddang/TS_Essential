7fb98d53c7329374628513a6b8302d1cfc948443
wukdddang <dnr8874@naver.com>
2024-03-05 00:53:38 +0900

    Feat: udemy ts 강의 학습 - 48 / 339

---
+++ features/classes.ts
@@ -0,0 +1,23 @@
+class Vehicle {
+  protected honk(): void {
+    console.log("beep");
+  }
+}
+
+const vehicle = new Vehicle();
+vehicle.honk();
+
+class Car extends Vehicle {
+  private drive(): void {
+    console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car();
+car.startDrivingProcess();
+car.honk();
