d86457e935a350002fc32fae7396d9fc9cd854df
wukdddang <dnr8874@naver.com>
2024-03-06 00:57:13 +0900

    Feat: udemy ts 강의 학습 - 56 / 339

--- features/classes.ts
+++ features/classes.ts
@@ -1,13 +1,19 @@
 class Vehicle {
+  constructor(public color: string) {}
+
   protected honk(): void {
     console.log("beep");
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.honk();
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
 
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
   private drive(): void {
     console.log("vroom");
   }
@@ -18,6 +24,5 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
+const car = new Car(4, "red");
 car.startDrivingProcess();
-car.honk();
