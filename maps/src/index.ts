64fae5986b7875f5fb08fc2eca27997f5be2e611
wukdddang <dnr8874@naver.com>
2024-03-18 00:07:27 +0900

    Feat: udemy ts 강의 학습 - 80 / 339

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,12 @@
 /// <reference types="@types/google.maps" />
 
 import { User } from "./User";
-// import { Company } from "./Company";
+import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
 
 const user = new User();
+const company = new Company();
 const customMap = new CustomMap("map");
 
-customMap.addUserMarker(user);
+customMap.addMarker(user);
+customMap.addMarker(company);
---
+++ sort/src/index.ts
@@ -0,0 +1,7 @@
+console.log("hi there!");
+
+const logSomething = () => {
+  console.log("something");
+};
+
+logSomething();
