2e071da42e4bea08ac18000b6ed1d12ea73476ba
wukdddang <dnr8874@naver.com>
2024-03-19 00:09:23 +0900

    Feat: udemy ts 강의 학습 - 87 / 339

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,21 @@
-console.log("hi there!");
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const logSomething = () => {
-  console.log("something");
-};
+  sort(): void {
+    const { length } = this.collection;
 
-logSomething();
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
