ea2311ada12468e9eedea9e874430afb125a55d2
wukdddang <dnr8874@naver.com>
2024-08-16 21:54:53 +0900

    Chore: 폴더 구조 변경

--- stats/build/reportTarget/ConsoleReport.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConsoleReport = void 0;
-class ConsoleReport {
-    print(report) {
-        console.log(report);
-    }
-}
-exports.ConsoleReport = ConsoleReport;
